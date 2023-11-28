f758b14b69d23c5659f5edc3742d530bab92368c
Einas1996 <EINAS.BAWABIJI@STUDENT.HU.NL>
2023-11-28 12:39:53 +0100

    functie herschrijven

--- src/index.ts
+++ src/index.ts
@@ -27,65 +27,87 @@ const people: Person[] = [
   print everyone's names
 */
 
-export function getNames() {
-  let result: string[] = [];
-  for (let i = 0; i < people.length; i++) {
-    result.push(people[i].name);
-  }
-  return result;
+//   export function getNames() {
+//   let result: string[] = [];
+//   for (let i = 0; i < people.length; i++) {
+//     result.push(people[i].name);
+//   }
+//   return result;
+// }
+
+// higher-order function
+export function getNames(): string[] {
+  return people.map(person => person.name);
 }
 
 /*
   filter by age
 */
 
-export function filterAge(limit: number) {
-  let result: Person[] = [];
-  for (let i = 0; i < people.length; i++) {
-    if (people[i].age < limit) {
-      result.push(people[i]);
-    }
-  }
-  return result;
-}
+// export function filterAge(limit: number) {
+//   let result: Person[] = [];
+//   for (let i = 0; i < people.length; i++) {
+//     if (people[i].age < limit) {
+//       result.push(people[i]);
+//     }
+//   }
+//   return result;
+// }
 
+export function filterAge(limit: number): Person[] {
+  return people.filter(person => person.age < limit);
+}
 /*
-  filter name by predicate 
+  filter name by predicate
 */
 
-export function filterNames(predicate: (str: string) => boolean) {
-  let result: Person[] = [];
-  for (let i = 0; i < people.length; i++) {
-    const person = people[i];
-    if (predicate(person.name)) {
-      result.push(person);
-    }
-  }
-  return result;
+// export function filterNames(predicate: (str: string) => boolean) {
+//   let result: Person[] = [];
+//   for (let i = 0; i < people.length; i++) {
+//     const person = people[i];
+//     if (predicate(person.name)) {
+//       result.push(person);
+//     }
+//   }
+//   return result;
+// }
+
+export function filterNames(predicate: (str: string) => boolean): Person[] {
+  return people.filter(person => predicate(person.name));
 }
 
 /*
   create greeting strings for all teachers
 */
 
-export function createGreetings() {
-  let greetings: string[] = [];
-  for (let i = 0; i < people.length; i++) {
-    const greeting = `Ik ben ${people[i].name} en ik ben ${people[i].age} jaar oud.`;
-    greetings.push(greeting);
-  }
-  return greetings;
+// export function createGreetings() {
+//   let greetings: string[] = [];
+//   for (let i = 0; i < people.length; i++) {
+//     const greeting = `Ik ben ${people[i].name} en ik ben ${people[i].age} jaar oud.`;
+//     greetings.push(greeting);
+//   }
+//   return greetings;
+// }
+
+export function createGreetings(): string[] {
+  return people.map(person => `Ik ben ${person.name} en ik ben ${person.age} jaar oud.`);
 }
+
 
 /*
   get the average age
 */
-export function getAverageAge() {
-  let sum = 0;
-  for (let i = 0; i < people.length; i++) {
-    sum += people[i].age;
-  }
+// export function getAverageAge() {
+//   let sum = 0;
+//   for (let i = 0; i < people.length; i++) {
+//     sum += people[i].age;
+//   }
 
+//   return sum / people.length;
+// }
+
+export function getAverageAge(): number {
+  const sum = people.reduce((accumulator, person) => accumulator + person.age, 0);
   return sum / people.length;
 }
 
